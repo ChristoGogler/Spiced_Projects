274a78e592ae18109102d251219de809dd9fa607
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-17 14:34:39 +0200

    dictation

---
+++ Week1/dictation.js
@@ -0,0 +1,20 @@
+var x;
+var doubleX;
+x = 53;
+
+function timesTwo(number) {
+    return number * 2;
+}
+
+doubleX = timesTwo(x);
+
+var numbers;
+numbers = [x, doubleX];
+
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+numbers = {};
+
+numbers.y = doubleX;
