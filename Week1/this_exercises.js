aa51069c98e1525c966f7e1ce39bd9e41bb4d7bc
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-17 16:26:12 +0200

    add exercise 2 - invertCase()

--- Week1/this_exercises.js
+++ Week1/this_exercises.js
@@ -32,3 +32,35 @@ console.log(rect.getArea()); //20
 
 var square = new Square(4);
 console.log(square.getArea()); //16
+
+// exercise 2
+
+//write function invertCase
+//1 parameter: string
+//invert uppercase/lowercase
+//return: another string
+
+function invertCase(string) {
+    var inverted = "";
+    for (var i = 0; i < string.length; i++) {
+        var currentCharacter = string.charAt(i);
+        //if UPPERCASE --> toLowerCase
+        //only works for alphabetic characters
+        if (/[A-Z]/.test(currentCharacter)) {
+            currentCharacter = currentCharacter.toLowerCase();
+        }
+        //else lowercase -->toUpperCase
+        //only works for alphabetic characters
+        else {
+            currentCharacter = currentCharacter.toUpperCase();
+        }
+        inverted += currentCharacter;
+    }
+
+    return inverted;
+}
+
+//test exercise 2
+var message = "Hej Muffin! How has your day been? Let's talk later at 3pm.";
+var invertedCasesMessage = invertCase(message);
+console.log(invertedCasesMessage);
