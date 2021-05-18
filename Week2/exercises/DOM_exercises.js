cc0e0f1a142eae2f26abafbb9d801cef0e39119c
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-18 22:50:47 +0200

    add exercise 1

--- Week2/exercises/DOM_exercises.js
+++ Week2/exercises/DOM_exercises.js
@@ -1,1 +1,25 @@
 console.log("DOM Selectors Exercise");
+
+//exercise 1
+//write function
+//find all the elements matching a selector
+//change their style to italic, underline & bold
+function findAndChange(selector) {
+    var elements = document.querySelectorAll(selector);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].innerHTML += " Yummy!";
+        elements[i].style.fontWeight = "bold";
+        elements[i].style.textDecoration = "underline";
+        elements[i].style.fontStyle = "italic";
+    }
+}
+
+//test exercise 1
+function testFindAndChange() {
+    findAndChange("h1"); //tag
+    findAndChange("#offer"); //id
+    findAndChange(".button"); //class
+}
+
+testFindAndChange();
