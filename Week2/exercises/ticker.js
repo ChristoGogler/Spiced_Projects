976e43ad6bf7e0a8dc7d2e4891f18a83f6a0f66f
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-19 15:32:46 +0200

    ticker -  Part 2

--- Week2/exercises/ticker.js
+++ Week2/exercises/ticker.js
@@ -1,40 +1,56 @@
 console.log("loaded: ticker.js");
 
-animateTicker();
-
-//underline on mouseenter
-function addHoverEffectToLinks() {}
-
-function animateTicker() {
+(function () {
+    //set up some variables
     var headlines = document.getElementById("headlines");
     var links = headlines.getElementsByTagName("a");
-    var widthOfHeadlines = headlines.offsetWidth;
-    var currentPosition = headlines.offsetLeft;
+    console.log("Links", links);
+    var currentPositionOfHeadlines = headlines.offsetLeft;
     var animationId;
     var SPEED = 2;
 
-    //console log checks
-    // console.log("links", links);
-    // console.log("widthOfHeadlines", widthOfHeadlines);
-    console.log("offsetLeft", currentPosition);
-    console.log(document.getElementById("headlines").style.left);
-
+    //function call to move the links on the screen
     moveLinks();
+    //function call to add event listerners
+    addHoverEffectToLinks();
 
+    //move the links on the screen
     function moveLinks() {
-        currentPosition -= SPEED;
-        // console.log("currentPosition:", currentPosition);
-        // console.log("-links[0].offsetWidth:", -links[0].offsetWidth);
-        if (currentPosition <= -links[0].offsetWidth) {
-            currentPosition += links[0].offsetWidth;
+        currentPositionOfHeadlines -= SPEED;
+
+        if (currentPositionOfHeadlines <= -links[0].offsetWidth) {
+            currentPositionOfHeadlines += links[0].offsetWidth;
             headlines.appendChild(links[0]);
         }
 
         //change position of headlines to new (current)position
-        //dont forget the unit
-        document.getElementById("headlines").style.left =
-            currentPosition + "px";
-        // console.log(document.getElementById("headlines").style.left);
+        //dont forget the unit "px"
+        headlines.style.left = currentPositionOfHeadlines + "px";
+        //also possible:
+        // document.getElementById("headlines").style.left =
+        //     currentPositionOfHeadlines + "px";
+
         animationId = requestAnimationFrame(moveLinks);
     }
-}
+
+    //add event listerners/handlers
+    function addHoverEffectToLinks() {
+        console.log("Links", links);
+        for (var i = 0; i < links.length; i++) {
+            //eventlistener mouseenter
+            links[i].addEventListener("mouseenter", function () {
+                console.log("Mouseenter occurred");
+                this.style.color = "blue";
+                this.style.textDecoration = "underline 2px";
+                cancelAnimationFrame(animationId);
+            });
+            //eventlistener mouseexit
+            links[i].addEventListener("mouseleave", function () {
+                console.log("Mouseleave occurred");
+                this.style.color = "white";
+                this.style.textDecoration = "none";
+                moveLinks();
+            });
+        }
+    }
+})();
