3f44d41500cfc745b3dc5f8d25e786f622e4c26f
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-20 16:51:19 +0200

    Canvas exercise 2 - stick person

--- Week2/exercises/Canvas_exercises.js
+++ Week2/exercises/Canvas_exercises.js
@@ -1,54 +1,85 @@
 console.log("Canvas Exercises");
 
 (function () {
-    var cnv = document.querySelector("canvas");
+    var SPEED = 20;
+    var STARTX = 100;
+    var STARTY = 150;
+    var innerCtx;
+    var outerCtx;
 
-    var ctx = cnv.getContext("2d");
+    setup();
+    function setup() {
+        innerCtx = getCanvasContext("innerCanvas", "2d");
+        drawOnInnerCanvas(innerCtx);
+        outerCtx = getCanvasContext("outerCanvas", "2d");
+        drawOnOuterCanvas(STARTX, STARTY);
+        addKeyListener();
+    }
 
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "seagreen";
-    ctx.fillStyle = "seagreen";
-    ctx.beginPath();
-    //draw head
-    //(x, y, radius, startAngle 0-2*PI, endAngle0-2*PI, anticlockwise)
-    //0 is "east"
-    ctx.arc(350, 70, 50, 0, 2 * Math.PI, false);
-    ctx.fill();
-    ctx.stroke(); //Now draw!
-    ctx.closePath(); //not necessary
+    function addKeyListener() {
+        //add eventlistener
+        document.addEventListener("keydown", function (event) {
+            if (event.code == "ArrowRight") {
+                clearCanvas(outerCtx);
+                drawOnOuterCanvas((STARTX += SPEED), STARTY);
+            }
+            if (event.code == "ArrowLeft") {
+                clearCanvas(outerCtx);
 
-    //draw neck &body
-    ctx.beginPath();
-    ctx.strokeStyle = "tomato";
-    ctx.moveTo(350, 120);
-    ctx.lineTo(350, 300); //what? Line! Where to? those coordinates!
-    ctx.stroke(); //Now draw!
+                drawOnOuterCanvas((STARTX -= SPEED), STARTY);
+            }
+        });
+    }
+    function drawOnInnerCanvas() {
+        innerCtx.lineWidth = 5;
+        innerCtx.strokeStyle = "seagreen";
+        innerCtx.fillStyle = "seagreen";
+        innerCtx.beginPath();
+        //draw head
+        //(x, y, radius, startAngle 0-2*PI, endAngle0-2*PI, anticlockwise)
+        //0 is "east"
+        innerCtx.arc(70, 70, 50, 0, 2 * Math.PI, false);
+        innerCtx.fill();
+        innerCtx.stroke(); //Now draw!
+        innerCtx.closePath(); //not necessary
 
-    //draw arms
-    ctx.moveTo(350, 150);
-    ctx.lineTo(390, 250);
-    ctx.moveTo(350, 150);
-    ctx.lineTo(310, 250);
+        //draw neck &body
+        innerCtx.beginPath();
+        innerCtx.strokeStyle = "tomato";
+        innerCtx.moveTo(70, 120);
+        innerCtx.lineTo(70, 300); //what? Line! Where to? those coordinates!
+        innerCtx.stroke(); //Now draw!
 
-    //draw legs
-    ctx.moveTo(350, 300);
-    ctx.lineTo(390, 500);
-    ctx.moveTo(350, 300);
-    ctx.lineTo(310, 500);
+        //draw arms
+        innerCtx.moveTo(70, 150);
+        innerCtx.lineTo(110, 250);
+        innerCtx.moveTo(70, 150);
+        innerCtx.lineTo(30, 250);
+        innerCtx.stroke();
+        //draw legs
+        innerCtx.beginPath();
+        innerCtx.strokeStyle = "blue";
+        innerCtx.moveTo(70, 300);
+        innerCtx.lineTo(110, 500);
+        innerCtx.moveTo(70, 300);
+        innerCtx.lineTo(30, 500);
 
-    ctx.stroke();
-
-    ctx.beginPath();
-
-    ctx.drawImage(document.querySelector("img"), 100, 350);
-
-    document.addEventListener("dblclick", function () {
-        ctx.clearRect(0, 0, 200, 200);
-    });
-
-    document.addEventListener("keydown", function (e) {
-        if (e.keyCode == 37) {
-            // left arrow
-        }
-    });
+        innerCtx.stroke();
+    }
+    function drawOnOuterCanvas(x, y) {
+        outerCtx.beginPath();
+        outerCtx.drawImage(document.querySelector("#innerCanvas"), x, y);
+    }
+    //function clearCanvas
+    //1 parameter: 1) canvasContext
+    function clearCanvas(ctx) {
+        ctx.clearRect(0, 0, 1500, 600);
+    }
+    //function getCanvasContext
+    //2 parameter: 1) string - id of the canvas element, 2) string - contextType (2d, webgl, etc)
+    //return: canvas context
+    function getCanvasContext(element, context) {
+        var cnv = document.getElementById(element);
+        return cnv.getContext(context);
+    }
 })();
