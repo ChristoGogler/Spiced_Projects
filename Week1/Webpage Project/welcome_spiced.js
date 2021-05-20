82c48106863cd8d8b82ce93c82c9d79090a22438
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-20 21:57:31 +0200

    Website Project: small fix, thanks Mufed! ;)

--- Week1/Webpage Project/welcome_spiced.js
+++ Week1/Webpage Project/welcome_spiced.js
@@ -3,7 +3,10 @@ console.log("welcome_SPICED.js");
 (function () {
     //add eventlistener to hamburger button
     var hamburgerButton = document.getElementById("hamburgerButton");
-    hamburgerButton.addEventListener("click", toggleNavigation);
+    hamburgerButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        toggleNavigation();
+    });
 
     function toggleNavigation() {
         var nav = document.querySelector("nav");
@@ -11,4 +14,18 @@ console.log("welcome_SPICED.js");
         document.body.classList.toggle("backdrop");
         hamburgerButton.firstElementChild.classList.toggle("close");
     }
+    //add eventlistener to esc key
+    document.addEventListener("keydown", function (event) {
+        if (event.code == "Escape") {
+            toggleNavigation();
+        }
+    });
+    //add eventlistener to document click exept nav
+    //add eventlistener - when user clicks
+    // document.body.addEventListener("click", function (event) {
+    //     toggleNavigation();
+    // });
+    // document.querySelector("nav").addEventListener("click", function (event) {
+    //     // event.stopPropagation();
+    // });
 })();
