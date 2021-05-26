63b2b068af7a233a47b655b566be6271aba38797
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-26 17:02:15 +0200

    add Jquery exercise 2 - modal popup

--- projects/1st Webpage/welcome_spiced.js
+++ projects/1st Webpage/welcome_spiced.js
@@ -1,12 +1,30 @@
 console.log("welcome_SPICED.js");
 
 (function () {
+    setTimeout(openModalPopup, 1000);
+
     //add eventlistener to hamburger button
     var hamburgerButton = document.getElementById("hamburgerButton");
     hamburgerButton.addEventListener("click", function (event) {
         event.stopPropagation();
         toggleNavigation();
     });
+    //add eventlistener to x button in modal
+    var $xButtonListener = $(".cancelButton").on("click", function () {
+        console.log("click");
+        closeModalPopup();
+    });
+
+    function openModalPopup() {
+        console.log("in open Modal");
+
+        var $modal = $(".modal").removeClass("hidden");
+        console.log($modal);
+    }
+    function closeModalPopup() {
+        var $modal = $(".modal").addClass("hidden");
+        console.log($modal);
+    }
 
     function toggleNavigation() {
         var nav = document.querySelector("nav");
