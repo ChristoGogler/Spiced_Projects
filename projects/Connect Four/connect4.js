9a0e0aaa34544ddc0ba87f0ec82a5e969cbd8f4c
CHRISTO GOGLER <christogogler@gmail.com>
2021-05-30 21:06:36 +0200

    Connect 4 - add comments, improve readability of code

--- projects/Connect Four/connect4.js
+++ projects/Connect Four/connect4.js
@@ -1,6 +1,5 @@
 // (function () {
-console.log("connect4.js -- CONNECT FOUR");
-// var rowsAndColumns = [];
+console.log("connect4.js -- CONNECT FOUR -- Christo Gogler");
 var $board = $(".board");
 var $columns = $board.find(".column");
 var $allSlots = $board.find(".slot");
@@ -31,7 +30,22 @@ var allDiagonals = [
     [3, 8, 13, 28],
 ];
 
+//Object Player
+//contains properties for each player
+function Player(id, name, $element, color, coin) {
+    this.id = id;
+    this.name = name;
+    this.$element = $element;
+    this.color = color;
+    this.coin = coin;
+    this.wins = 0;
+}
+
 initNewGame();
+//initNewGame
+// 0 parameter
+//do: initialise the game with players
+//no return
 function initNewGame() {
     //create Player objects
     yin = new Player("yin", "Sandy", $playerYin, "palevioletred", "yinCoin");
@@ -45,38 +59,45 @@ function initNewGame() {
     nextPlayer();
 }
 
-function placeCoin($slots) {
-    //place coin in the first free slot
+//dropCoin
+// 1 parameter: 1) $slots - slots of clicked column
+//do: place coin in the first free slot
+//return: i - index of the row the coin dropped into
+function dropCoin($slots) {
     //check for first free slot
     for (var i = $slots.length - 1; i >= 0; i--) {
         var isTaken =
             $slots.eq(i).hasClass(yin.coin) || $slots.eq(i).hasClass(yang.coin);
-        //if its not taken add class currentPlayer.id
+        //if its not taken add class currentPlayer.coin and return index
         if (!isTaken) {
             $slots.eq(i).addClass(currentPlayer.coin);
             coinsPlaced++;
             return i;
         }
     }
+    //no index found/all taken
     return -1;
 }
 
+// nextPlayer
+//0 parameters
+//do: switch to the next player
+//no return
 function nextPlayer() {
     if (currentPlayer === yin) {
         currentPlayer = yang;
-        // console.log(currentPlayer.$element);
         $playerYin.removeClass("yourTurn");
     } else {
         currentPlayer = yin;
-        // console.log(currentPlayer.$element);
         $playerYang.removeClass("yourTurn");
     }
     currentPlayer.$element.addClass("yourTurn");
 }
 
 //checkVictoryPreConditions
-//parameter:
-//return:
+//2 parameter: 1) $slots of the clicked column, 2) index of the row the last coin dropped into
+//do: check for a few preconditions that need to be met for a victory check
+//return: boolean expression, true if at least 1 precondition is met
 function checkVictoryPreConditions($slots, rowIndex) {
     //1) there must be at least 7 coins placed on the board
     if (coinsPlaced < 7) {
@@ -108,59 +129,52 @@ function checkVictoryPreConditions($slots, rowIndex) {
         }
 
         //4) check for DIAGONAL CONDITION
-        //there must be at least 10 coins placed
-        //there must be at least one coin in row 2 & one coin in row 3 ( column index one left/right of coin in row2)
+        //a) there must be at least 10 coins placed
+
         if (coinsPlaced > 9) {
             possibleDiagonalWin = true;
         }
+        //b) there must be at least one coin in row 2 & one coin in row 3 ( column index one left/right of coin in row2)
+        //...not implemented yet
     }
 
     return possibleColumnWin || possibleRowWin || possibleDiagonalWin;
 }
+
 //checkVictory
-//parameter:
-//return:
+//1 parameter: 1) a collection of slots/positions
+//No return
 function checkVictory($positions) {
     $winningSlots = $();
-    var count = 0;
+    //loop over all slots and to winning slots if they contain coins belonging to current player
     for (var i = 0; i < $positions.length; i++) {
-        // console.log("Iteration", i);
         if ($positions.eq(i).hasClass(currentPlayer.coin)) {
             $winningSlots = $winningSlots.add($positions.eq(i));
-            count++;
-            console.log(
-                "Add slot to winningCombo",
-                i,
-                "length",
-                $winningSlots.length
-            );
-            if (count == 4) {
-                // console.log(currentPlayer.id, "WIN!");
-                // console.log(i, "in checkVictory $winningSlots", $winningSlots);
+
+            //if 4 consecutive winning slots have been found, announce the winner
+            //otherwise reset winning slots
+            if ($winningSlots.length == 4) {
                 winner = currentPlayer;
-                announceWinner($winningSlots, winner);
+                announceWinner();
             }
         } else {
-            count = 0;
             $winningSlots = $();
-            console.log("reset winningcombo", $winningSlots);
         }
     }
 }
 
-function Player(id, name, $element, color, coin) {
-    this.id = id;
-    this.name = name;
-    this.$element = $element;
-    this.color = color;
-    this.coin = coin;
-    this.wins = 0;
-}
-
+//isFull
+//1 parameter: 1) index
+//check if the column is already filled up
+//return: boolean expression, true if full
 function isFull(index) {
     return index < 0;
 }
 
+//getRow
+//1 parameter: 1) index of the row that the coin dropped into
+//do: create a jquery collection of slots that can be checked for a win
+//return: collection of slots in that row
 function getRow(index) {
     var $slotsInRow = $();
     for (var i = 0; i < $columns.length; i++) {
@@ -169,39 +183,57 @@ function getRow(index) {
     return $slotsInRow;
 }
 
+//getDiagonals
+//1 parameter: 1) indexes of a hardcoded array of all possible diagonals
+//do: create a jquery collection of slots that can be checked for a win
+//return: collection of slots
 function getDiagonal(indexes) {
     var $diagonal = $();
+    //loop over all indexes and add all its elements
     for (var i = 0; i < indexes.length; i++) {
         $diagonal = $diagonal.add($allSlots.eq(indexes[i]));
     }
     return $diagonal;
 }
-
-function announceWinner($winningSlots, winner) {
+//announceWinner
+//0 parameter
+// do: stop the round and announce the winner
+//no return
+function announceWinner() {
+    //show the winning combo
     for (var i = 0; i < $winningSlots.length; i++) {
         $winningSlots.eq(i).addClass("winningCombo");
     }
+    //swap name for "Winner"
     winner.$element.find(".name").html("Winner");
+    //add 1 to winner's count and show wins
     winner.wins++;
     winner.$element.find(".wins").html("won " + winner.wins);
-    //show message
+    //show message and reset button
     var $message = $(".message");
     $message.find("div").html(winner.name + " has won!");
     $message.css("background-color", winner.color).css("display", "flex");
-
-    //show resetButton
 }
 
+//resetGame
+//0 parameter
+// do: reset board to initial state
+//no return
 function resetGame() {
+    //remove all coins from board
     for (var element of $allSlots) {
         $(element).removeClass(yin.coin);
         $(element).removeClass(yang.coin);
         $(element).removeClass("winningCombo");
     }
+    //reset the winners name
     winner.$element.find(".name").html(winner.name);
+
     winner = null;
+    //remove the modal dialog
     $(".message").css("display", "none");
 }
+
 //RESETBUTTON CLICK EVENT LISTENER
 $(".resetButton").on("click", function () {
     resetGame();
@@ -219,7 +251,7 @@ function columnClickHandler($clickedColumn) {
     var $slotsInColumn = $clickedColumn.find(".slot");
     //get index of columns and rows
     var columnIndex = $columns.index($clickedColumn);
-    var rowIndex = placeCoin($slotsInColumn);
+    var rowIndex = dropCoin($slotsInColumn);
     var $winningSlots = [];
 
     //as long as a column is not completely occupied by coins
@@ -244,12 +276,11 @@ function columnClickHandler($clickedColumn) {
         //checkVictory for diagonal
         if (possibleDiagonalWin) {
             for (var diagIndexes of allDiagonals) {
-                // allDiagonals.forEach(function (diagIndexes) {
                 var $diagonal = getDiagonal(diagIndexes);
                 checkVictory($diagonal);
-                // });
             }
         }
+        //call nextPlayer if theres no winner yet
         if (!winner) {
             nextPlayer();
         }
