00b2c83f75fe7033be333bd9bba486c464650bca
CHRISTO GOGLER <christogogler@gmail.com>
2021-06-02 16:47:01 +0200

    add .js event listener & handler

--- projects/Spotify Search/spotify.js
+++ projects/Spotify Search/spotify.js
@@ -6,8 +6,9 @@
     var $submitButton = $("#submitButton");
     var $resultsHeadline = $("searchResultsheadline");
     var $listOfResults = $("#searchResults");
-
     var $moreButton = $("#moreButton");
+
+    const URL = "https://spicedify.herokuapp.com/spotify";
 
     console.log(
         "cache: ",
@@ -18,4 +19,41 @@
         $listOfResults,
         $moreButton
     );
+
+    //SUBMIT BUTTON LISTENER
+    $submitButton.on("click", function () {
+        console.log("Search CLICK");
+        var userinput = $input.val();
+        var searchType = $type.val();
+
+        performRequest(userinput, searchType);
+    });
+
+    //MORE BUTTON LISTENER
+    $moreButton.on("click", function () {
+        console.log("More CLICK");
+    });
+
+    //performRequest
+    // 2 parameters: 1) userInput - what the user is searching for, 2) searchtype - album or artist
+    //create ajax request
+    //no return
+    function performRequest(userinput, searchType) {
+        $.ajax({
+            url: URL,
+            data: {
+                q: userinput,
+                type: searchType,
+            },
+            success: function (results) {
+                renderResults(results);
+            },
+        });
+    }
+
+    //renderResults
+    // 1 parameters: 1) results - what the server returns as a result
+    //make things appear in the results list
+    //no return
+    function renderResults(results) {}
 })();
