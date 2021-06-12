eeed8a11d5e229bbf30e13539fedffb21b7dd7e4
CHRISTO GOGLER <christogogler@gmail.com>
2021-06-12 15:23:12 +0200

    add comments - express exercise

--- projects/express_project/index.js
+++ projects/express_project/index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-
 //instance of express!
 const app = express();
 
@@ -11,13 +10,14 @@ app.post(
     `/cookie`,
     bodyParser.urlencoded({ extended: false }),
     (request, response, next) => {
+        //if accepted --> let move on to desired page
         if (request.body.acceptCookie) {
             console.log(`Setting chocolateChip cookie...!`);
             response.cookie("chocolateChip", true);
-            // request.cookies.chocolateChip = true;
             response.redirect(request.cookies.desiredUrl);
             return;
         }
+        //otherwise redirect to "must approve! page"
         response.redirect("/must-approve-cookie");
     }
 );
@@ -31,7 +31,7 @@ app.use(checkCookie)
     //make static content available that is in the "public" folder
     .use(express.static(__dirname + "/public"))
     .use(express.urlencoded({ extended: false }));
-
+//homepage
 app.get(`/`, (request, response) => {
     response.send("HOMEPAGE");
 });
@@ -41,10 +41,15 @@ app.listen(8080, () =>
     console.log(`---- EXPRESS SERVER .... listening .... ----`)
 );
 
+//setCookie
+//3 parameters: response, property name, value
+//do: save a new cookie in response with given parameters
 function setCookie(response, property, value) {
     response.cookie(property, value);
 }
 
+//getMustApprovePage
+// parameter: response
 function getMustApprovePage(response) {
     response.send(`<!DOCTYPE html>
 <html lang="en">
@@ -58,6 +63,8 @@ function getMustApprovePage(response) {
 `);
 }
 
+//getCookiePage
+// parameter: response
 function getCookiePage(response) {
     response.send(`<!DOCTYPE html>
 <html lang="en">
@@ -75,16 +82,18 @@ function getCookiePage(response) {
 `);
 }
 
+//checkCookie
+//3 parameters: request, response, next
+//do: check if user has accepted the cookie policy, if not redirect to acceptance page
+//otherwise let user move on to requested page
 function checkCookie(request, response, next) {
-    console.log("[cookies]", request.cookies);
+    console.log(`all my cookies: ${request.cookies}`);
+    //save requested url for later
     setCookie(response, "desiredUrl", request.url);
-    desiredUrl = request.url;
 
     if (request.cookies.chocolateChip) {
         //-->redirect to desired page
         console.log("Welcome back, Buiscuit!");
-        console.log("URL", request.url);
-        // response.redirect(request.url);
         next();
         return;
     }
