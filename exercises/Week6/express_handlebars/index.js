0bc19865ea396d67803e6500c9e24fb6a7985f77
CHRISTO GOGLER <christogogler@gmail.com>
2021-06-15 14:48:29 +0200

    add basic description page

--- exercises/Week6/express_handlebars/index.js
+++ exercises/Week6/express_handlebars/index.js
@@ -7,8 +7,9 @@ const handleBars = require("express-handlebars");
 const path = require("path");
 
 //get json data!
-console.log(__dirname);
+//console.log(__dirname);
 const projects = require(path.join(__dirname, "public/data/projects.json"));
+//console.log(typeof projects);
 const cats = require(path.join(__dirname, "public/data/cats.json"));
 
 //set up handlebars as template engine
@@ -18,6 +19,7 @@ app.set("view engine", "handlebars");
 //make static content available that is in the "public" folder
 app.use(express.static(path.join(__dirname + "/public")));
 
+//get the overview page
 app.get("/", (request, response) => {
     response.render("portfolio", {
         layout: "main",
@@ -26,6 +28,34 @@ app.get("/", (request, response) => {
         projects,
     });
 })
+    //get the individual project description page
+    .get(`/project/:project_name`, (request, response) => {
+        const { project_name } = request.params;
+
+        let project;
+        console.log("Project name", project_name);
+        // find the project that matches projName in the projects array
+        for (const element of projects) {
+            console.log(element.directory);
+            if (element.directory === project_name) {
+                project = element;
+                console.log(`...rendering ${project}`);
+                // if no matching project is found, send 404
+                response.render("singleproject", {
+                    layout: "main",
+                    style: "single.css",
+                    title: "Project Title",
+                    project,
+                    projects,
+                });
+                return;
+            }
+        }
+        console.log("couldnt find anything! 404!");
+        response.sendStatus(404);
+        return;
+    })
+    //get the test page with cats
     .get("/hello", (request, response) => {
         response.render("hello", {
             layout: "main",
