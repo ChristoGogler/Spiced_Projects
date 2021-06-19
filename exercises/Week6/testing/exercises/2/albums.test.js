e4c3c8ff6fcddf6765b7d2f5ecd6ada05a35f83d
CHRISTO GOGLER <christogogler@gmail.com>
2021-06-19 15:18:24 +0200

    add exercise 2

--- exercises/Week6/testing/exercises/2/albums.test.js
+++ exercises/Week6/testing/exercises/2/albums.test.js
@@ -3,7 +3,29 @@ const spotify = require("./spotify");
 
 jest.mock("./spotify");
 
+
+
+
 test("album names are in alphabetical order", () => {
+
+spotify.search.mockResolvedValue({
+    "albums": {
+            "items": [
+                {
+                "name": "b",
+                },
+                {
+                "name": "a",
+                },
+                {
+                "name": "c",
+                }
+            ],
+    },
+
+});
+
+
     return getAlbumNames("meat loaf").then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
